85744cbd219744b49bd2da2a00291cad3c522793
Karan Singh <godthatback21@gmail.com>
2026-08-23 10:44:50 +0530

    agent added

--- test.py
+++ test.py
@@ -1,4 +1,11 @@
 from tools.tavily_tool import tavily_search
+from baclend import run_travel_agent
 
-res = tavily_search("Best hotels in India")
-print(res)
+user_input = input("Enter your query :")
+
+response = run_travel_agent(
+    user_input=user_input,
+    thread_id="test"
+)
+
+print(response["answer"])
